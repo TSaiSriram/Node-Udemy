5e3d2c48de06dcb172a7dc27cfa184d530d7e8f8
TSaiSriam <tekumallasaisriram@gmail.com>
2018-12-04 12:58:55 +0000

    autocommit 12.04.2018_12:58:55

--- mygit.sh
+++ mygit.sh
@@ -1,9 +1,9 @@
 now=$(date +"%m.%d.%Y_%T")
 dir=$("/projects/node-udemy/")
 cd $dir
-git add .
+#git add .
 git commit -am "autocommit $now"
 git push origin master
-git add *
-git commit -a -m "autoupdate `date +%F-%T`"
-git push
+#git add *
+#git commit -a -m "autoupdate `date +%F-%T`"
+#git push
