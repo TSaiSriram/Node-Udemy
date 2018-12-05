76f754704d98f9f51805843c4f5643e110a426d1
TSaiSriam <tekumallasaisriram@gmail.com>
2018-12-05 09:53:41 +0000

    Auto commited $date

--- mygit.sh
+++ mygit.sh
@@ -1,1 +1,1 @@
-cd /projects/node-udemy && git add -A && git commit * --allow-empty-message -m '' && git push 
+cd /projects/node-udemy && git add -A && git commit *  -m ' Auto commited $date ' && git push 
